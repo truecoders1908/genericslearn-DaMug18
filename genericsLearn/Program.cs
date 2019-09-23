5fff5d5ea04afdef1fbab1e8acf6f5c22ae98840
Ryan Smith <Rwsmith8692@gmail.com>
2019-09-23 10:36:56 -0500

    Update Program.cs

--- genericsLearn/Program.cs
+++ genericsLearn/Program.cs
@@ -6,34 +6,37 @@ namespace genericsLearn
     class Program
     {
         static void Main(string[] args)
+        // Generics allow you to define a class with placeholders for the type of its fields, methods, parameters, etc. Generics replace these placeholders with some specific type at compile time.
+        // Generics is a technique that improves your programs in many ways such as: It helps you in code reuse, performance and type safety. You can create your own generic classes, methods, interfaces and delegates. You can create generic collection classes.
         {
-            Bag<char> scrabbleLetters = new Bag<char>();
-            scrabbleLetters.PutItemIntoBag('q');
-            scrabbleLetters.PutItemIntoBag('j');
-            scrabbleLetters.PutItemIntoBag('z');
-            scrabbleLetters.PutItemIntoBag('y');
-            scrabbleLetters.PutItemIntoBag('x');
-            char letterFromBag = scrabbleLetters.GetItemFromBag();
-            Console.WriteLine(letterFromBag);
+            Bag<char> scrabbleLetters = new Bag<char>();        // Creating a new instance of Bag named scrabbleLetters
+            scrabbleLetters.PutItemIntoBag('q');                // scrabbleLetters == q
+            scrabbleLetters.PutItemIntoBag('j');                // scrabbleLetters == j
+            scrabbleLetters.PutItemIntoBag('z');                // scrabbleLetters == z
+            scrabbleLetters.PutItemIntoBag('y');                // scrabbleLetters == y
+            scrabbleLetters.PutItemIntoBag('x');                // scrabbleLetters == x
+            char letterFromBag = scrabbleLetters.GetItemFromBag();      // Defines scrabbleLetters method GetItemFromBag [line 31] as letterFromBag
+            Console.WriteLine(letterFromBag);                           // Console will display a random scrabble piece from bag using the list (q,j,z,y,x)
         }
     }
 
-    class Bag<T>
+    class Bag<T>                // New Class: Bag with <T> being used as a place holder
     {
-        private List<T> items = new List<T>();
+        private List<T> items = new List<T>();      //[private] is only going to be shared inside this nest thats creating a new bag list instance
 
-        public void PutItemIntoBag(T item)
-        {
-            items.Add(item);
+        public void PutItemIntoBag(T item)          // New Method: public void PutItemIntoBag(T item)
+        {                                           // ***Bag = T***
+
+            items.Add(item);                        // Item is being placed into the bag
         }
 
-        public T GetItemFromBag()
+        public T GetItemFromBag()                   // Method to gets items from bag
         {
-            Random rnd = new Random();
-            int randomIndex = rnd.Next(0, items.Count);
-            T randomItem = items[randomIndex];
-            items.RemoveAt(randomIndex);
-            return randomItem;
+            Random rnd = new Random();                  // Random Functionality to grab random scrabble letter
+            int randomIndex = rnd.Next(0, items.Count);         // Counts amount of items inside bag
+            T randomItem = items[randomIndex];              // Creating a variable random that equals randomindex
+            items.RemoveAt(randomIndex);                    // Removes letter from the Bag
+            return randomItem;                              // Random item that was grabbed from bag then will be displayed on Console.WriteLine [Line 17]
         }
     }
 }
